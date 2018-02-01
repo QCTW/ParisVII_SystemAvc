a8ce316a6013518023b9d316c5acfedcaec674d9
Quincy <quincy.tw@gmail.com>
2018-02-01 21:47:12 +0000

    Copy executable to jailed chroot

--- HsiehYK_TD1.c
+++ HsiehYK_TD1.c
@@ -12,9 +12,75 @@
 #include <sys/utsname.h>
 #include <sys/stat.h>
 
+#include <fcntl.h>
+#include <errno.h>
+
+/**File copy source:
+   https://stackoverflow.com/questions/2180079/how-can-i-copy-a-file-on-unix-using-c **/
+
+int cp(const char *from, const char *to)
+{
+    int fd_to, fd_from;
+    char buf[4096];
+    ssize_t nread;
+    int saved_errno;
+
+    fd_from = open(from, O_RDONLY);
+    if (fd_from < 0)
+        return -1;
+
+    fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, 0550);
+    if (fd_to < 0)
+        goto out_error;
+
+    while (nread = read(fd_from, buf, sizeof buf), nread > 0)
+    {
+        char *out_ptr = buf;
+        ssize_t nwritten;
+
+        do {
+            nwritten = write(fd_to, out_ptr, nread);
+
+            if (nwritten >= 0)
+            {
+                nread -= nwritten;
+                out_ptr += nwritten;
+            }
+            else if (errno != EINTR)
+            {
+                goto out_error;
+            }
+        } while (nread > 0);
+    }
+
+    if (nread == 0)
+    {
+        if (close(fd_to) < 0)
+        {
+            fd_to = -1;
+            goto out_error;
+        }
+        close(fd_from);
+        /* Success! */
+        return 0;
+    }
+
+  out_error:
+    saved_errno = errno;
+    close(fd_from);
+    if (fd_to >= 0)
+        close(fd_to);
+
+    errno = saved_errno;
+    return -1;
+}
+
+
 int fn(void *arg)
 {
     printf("Pid of cloned instance: %d Parent's pid: %d\n", getpid(), getppid());
+    
+    //Set machine name
     const char newHostName[9] = "cloned-vm";
     size_t n = sizeof(newHostName);
     if(sethostname("cloned-vm", n) != 0)
@@ -22,24 +88,63 @@ int fn(void *arg)
         perror("Calling sethostname('cloned-vm')");
         exit(EXIT_FAILURE);
     }
+
     struct utsname unameData;
     if (uname(&unameData) != 0)
     {
         perror("Calling utsname");
         exit(EXIT_FAILURE);
     }
+
+    //Show uname -a
     printf("uname -a: %s %s %s %s %s %s.\n", unameData.sysname, unameData.nodename, unameData.release, unameData.version, unameData.machine, unameData.domainname);
     struct stat st = {0};
-    const char vmRootDir[9] = "./vm_root";
+    const char vmRootDir[9] = "vm_root";
     if (stat(vmRootDir, &st) == -1) 
     {
         if(mkdir(vmRootDir, 0700) !=0 )
         {
-            perror("Calling mkdir('./vm_root')");
+            perror("Calling mkdir('vm_root')");
             exit(EXIT_FAILURE);
         }
     }
-    return 0;
+
+    const char vmRootBinDir[13] = "vm_root/bin";
+    //Create /bin and copy utility executables under vm_root/bin
+    if (stat(vmRootBinDir, &st) == -1)
+    {
+         if (mkdir(vmRootBinDir, 0700) !=0)
+         {
+	     perror("Calling mkdir('vm_root/bin')");
+             exit(EXIT_FAILURE);
+         }
+         cp("/bin/bash", "vm_root/bin/bash");
+         cp("/bin/ls", "vm_root/bin/ls");
+         cp("/bin/cat", "vm_root/bin/cat");
+         cp("/bin/cp", "vm_root/bin/cp");
+    }
+
+    //Limit directory under ./vm_root
+    if (chroot(vmRootDir) ==-1)
+    {
+	perror("Calling chroot('vm_root')");
+        exit(EXIT_FAILURE);
+    }
+    if (chdir("/") ==-1)
+    {
+        perror("Calling chdir('vm_root')");
+        exit(EXIT_FAILURE);
+    }
+    
+    //Execute ls
+    char *arguments[] = { "ls", NULL };
+    if (execv("/bin/ls", arguments) == -1) 
+    {
+	perror("Calling execv('/bin/ls')");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
 
 
@@ -59,5 +164,5 @@ int main( int argc, const char* argv[] )
     }
     wait(NULL);
     free(child_stack);
-    printf("INFO: Child process terminated.\n"); 
+    printf("INFO: Child process terminated.\n");
 }
